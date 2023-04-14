4bbcacaf7136da118a72b5078f8838cc7113b465
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-04-14 20:00:56 +0300

    Add files via upload

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Emre Ülkem on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
